e189bc51eb13899c876311d38b4d1286a505ce67
Docvoids <143121183+Docvoids@users.noreply.github.com>
2025-06-20 03:12:39 +0300

    Update ast_nodes.py
    Add nodes for binary operations assignment

--- LOLpython/ast_nodes.py
+++ LOLpython/ast_nodes.py
@@ -3,30 +3,24 @@ from typing import Any, List, Optional
 
 @dataclass
 class ASTNode: pass
-
 @dataclass
 class ExpressionNode(ASTNode): pass
-
 @dataclass
-class LiteralNode(ExpressionNode):
-    value: Any
-
+class LiteralNode(ExpressionNode): value: Any
 @dataclass
-class IdentifierNode(ExpressionNode):
-    name: str
-
+class IdentifierNode(ExpressionNode): name: str
+@dataclass
+class BinaryOpNode(ExpressionNode): # <-- Новий вузол
+    left: ExpressionNode
+    op: str
+    right: ExpressionNode
 @dataclass
 class StatementNode(ASTNode): pass
-
 @dataclass
-class ProgramNode(ASTNode):
-    statements: List[StatementNode]
-
+class ProgramNode(ASTNode): statements: List[StatementNode]
 @dataclass
-class VarDeclNode(StatementNode):
-    name: str
-    initializer: Optional[ExpressionNode]
-
+class VarDeclNode(StatementNode): name: str; initializer: Optional[ExpressionNode]
 @dataclass
-class VisibleNode(StatementNode):
-    expressions: List[ExpressionNode]
+class AssignmentNode(StatementNode): target: ExpressionNode; expression: ExpressionNode
+@dataclass
+class VisibleNode(StatementNode): expressions: List[ExpressionNode]
